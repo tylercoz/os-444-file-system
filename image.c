2f1af7ad74ef903e67c3c86f7f629144b8dd43cd
Abbey Hankins <abbeyhankins57@gmail.com>
2025-04-30 12:18:43 -0700

    building image.c and image.h

---
+++ image.c
@@ -0,0 +1,13 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include "image.h"
+
+int image_fd; 
+
+int image_open(char *filename, int truncate){
+    image_fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+    return image_fd;
+}
+int image_close(void){
+    return close(image_fd);
+}
