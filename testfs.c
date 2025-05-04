2cd3ade9b14e51e4234bd3100881e774ff03d4f0
Abbey Hankins <abbeyhankins57@gmail.com>
2025-05-04 11:56:05 -0700

    fixing test

--- testfs.c
+++ testfs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "ctest.h"
+#include "image.h"
 
 // Testing
 #ifdef CTEST_ENABLE
@@ -13,23 +14,8 @@ void test_testing(int x, int y) {
   CTEST_ASSERT(1, "testing testing 4");
   CTEST_ASSERT(1, "testing testing 5");
 }
-
-int main() {
-  CTEST_VERBOSE(1);
-
-  test_testing(1, 4);
-
-  CTEST_RESULTS();
-
-  CTEST_EXIT();
-}
-
-// Production
-#else
-
-
 void test_image_open(){
-  char *test_file = "test_file.txt"
+  char *test_file = "test_file.txt";
 
   //open file no truncate 
   image_fd = image_open(test_file, 0);
@@ -46,6 +32,24 @@ void test_image_open(){
 void test_image_close(){
   CTEST_ASSERT(image_close() == 0, "file should close");
 }
+
+int main() {
+  CTEST_VERBOSE(1);
+
+  test_testing(1, 4);
+
+  test_image_open();
+  test_image_close();
+
+
+  CTEST_RESULTS();
+
+  CTEST_EXIT();
+}
+
+// Production
+#else
+
 
 int main() {
   printf("hello, mother!\n");
