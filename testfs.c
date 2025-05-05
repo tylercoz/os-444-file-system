958b937589eee4e2ea7cb636326199a9ff3ac110
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2025-05-04 21:48:50 -0700

    removed useless code

--- testfs.c
+++ testfs.c
@@ -5,27 +5,18 @@
 // Testing
 #ifdef CTEST_ENABLE
 
-void test_testing(int x, int y) {
-  (void) x;
-  (void) y;
-  CTEST_ASSERT(1, "testing testing 1");
-  CTEST_ASSERT(1, "testing testing 2");
-  CTEST_ASSERT(1, "testing testing 3");
-  CTEST_ASSERT(1, "testing testing 4");
-  CTEST_ASSERT(1, "testing testing 5");
-}
 void test_image_open(){
   char *test_file = "test_file.txt";
 
-  //open file no truncate 
+  //open file no truncate
   image_fd = image_open(test_file, 0);
   CTEST_ASSERT(image_fd >= 0, "file open should succeed");
-  image_close(); 
+  image_close();
 
-  //open file yes truncate 
+  //open file yes truncate
   image_fd = image_open(test_file, 1);
   CTEST_ASSERT(image_fd >= 0, "file open should succeed");
-  image_close(); 
+  image_close();
 
 }
 
@@ -35,8 +26,6 @@ void test_image_close(){
 
 int main() {
   CTEST_VERBOSE(1);
-
-  test_testing(1, 4);
 
   test_image_open();
   test_image_close();
