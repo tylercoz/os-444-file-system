7187f537c751a966201dd7084882f2ed7ba44700
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2025-05-04 22:19:50 -0700

    open() assigns file descriptor to global var image_fd

--- image.c
+++ image.c
@@ -4,15 +4,20 @@
 
 int image_fd;
 
+// returns file descriptor
 int image_open(char *filename, int truncate){
   if (truncate) {
-    return open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+    image_fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
   }
-  return open(filename, O_RDWR | O_CREAT, 0600);
+  else {
+    image_fd = open(filename, O_RDWR | O_CREAT, 0600);
+  }
+  return image_fd;
 }
-int image_close(void){  
+
+int image_close(void){
     if (image_fd != -1){
       close(image_fd);
     }
-    return 0; 
+    return 0;
 }
